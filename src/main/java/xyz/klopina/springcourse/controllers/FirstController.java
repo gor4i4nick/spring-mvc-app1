89c3020e306bd37fabd3096178c4a7fe42707c47
Mykola Radchenko <klopina@i.ua>
2021-04-23 07:35:14 +0300

    request with parameters

--- src/main/java/xyz/klopina/springcourse/controllers/FirstController.java
+++ src/main/java/xyz/klopina/springcourse/controllers/FirstController.java
@@ -3,23 +3,25 @@ package xyz.klopina.springcourse.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
+
     @GetMapping("/hello")
-    public String helloPage(HttpServletRequest request){
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
 
         System.out.println("Hello, " + name + " " + surname);
 
         return "first/hello";
     }
-@GetMapping("/goodbye")
-    public String goodbyePage(){
+
+    @GetMapping("/goodbye")
+    public String goodbyePage() {
         return "first/goodbye";
     }
 }
