c89f1472d47e948d8ebb5ed5113f5fe2d5379ac6
Mykola Radchenko <klopina@i.ua>
2021-04-24 09:01:48 +0300

    Commit 1 из текстового редактора

--- src/main/java/xyz/klopina/springcourse/controllers/FirstController.java
+++ src/main/java/xyz/klopina/springcourse/controllers/FirstController.java
@@ -14,7 +14,6 @@ public class FirstController {
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
                             @RequestParam(value = "surname", required = false) String surname) {
-
         System.out.println("Hello, " + name + " " + surname);
 
         return "first/hello";
